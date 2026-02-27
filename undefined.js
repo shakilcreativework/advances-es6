0702ff38af7c0d9904258b4a94bb61420bec3e57
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-27 15:19:30 +0600

    amazing, learning was undefined and null undefined meaning any mistake and null sell me

---
+++ undefined.js
@@ -0,0 +1,18 @@
+let data;
+// console.log(data);
+
+const sum = (a, b) => {
+    console.log(a, b);
+};
+
+// sum(4);
+
+
+const student = {
+    name: 'Shakil Ahmed',
+    age: 29,
+    salary: null,
+};
+
+console.log(student.age);
+console.log(student.marks);
