e588f57ae80520eb664039995c20a93f19a9ec9a
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-27 15:49:31 +0600

    compares duble equal and triple equal

---
+++ duble-equals.js
@@ -0,0 +1,10 @@
+console.log(2==2);
+console.log(2=='2');
+console.log(1==true);
+console.log(0==false);
+console.log(true=='1');
+console.log(false=='0');
+console.log(null==undefined);
+console.log(NaN == NaN);
+console.log([5] == '5');
+console.log({} == {});
