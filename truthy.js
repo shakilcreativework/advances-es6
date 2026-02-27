345c1838013728ded07a495c8b75f657c9416ace
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-27 15:30:11 +0600

    check truthy or falsy using ! and !!

---
+++ truthy.js
@@ -0,0 +1,17 @@
+let data;
+data = 0;
+data = '';
+data = '0';
+data = false;
+data = true;
+data = null;
+data = undefined;
+data = {};
+data = [];
+
+if(data){
+    console.log('Truthy');
+}else{
+    console.log('Falsy');
+}
+
